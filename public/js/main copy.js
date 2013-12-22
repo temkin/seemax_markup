2c63f8cae8717115fa98c1ee829dea55b4d270ca
aeliriannon <iriska8807@gmail.com>
2013-12-22 11:42:45 +0300

    second page

--- public/js/main copy.js
+++ public/js/main copy.js
@@ -1,129 +1,3 @@
-<<<<<<< HEAD
-$(document).ready(function(){
-
-    /* Показать/Спрятать поиск */
-    $(".search i").click(function(){
-         $(".header__search").show();
-        setTimeout(function(){
-            $(".close_ar").addClass("show");
-        },400);
-         $(".header__i").hide();
-     });
-     $(".close_cross").click(function(){
-         $(this).removeClass("show");
-         $(".header__search").hide();
-         $(".header__i").show();
-     });
-
-    /* закрытие поиска по клику вне блока поиска */
-    $(document).on("mousedown", function(e){
-        if($(e.target).closest(".header__search").length != 1){
-            if($(".close_ar").hasClass("show")){
-                $(".close_ar").trigger("click");
-            }
-        }
-    });
-
-
-    /*** Slider Handler ***/
-
-    var sliderInterval = 5000;
-    var sliderElementsNum = $('.slider__ii').children().length;
-    var sliderActive = 1;
-
-    
-    // Animate nav images: zoom in/out
-
-    function navZoomOut(navObj) // `nav__i` -> navObj
-    {
-        navObj.find(".boxImg").stop(true, false).animate({
-            top: -2,
-            left: -0,
-            width: 82,
-            height: 82
-        }, 400);
-    }
-
-    function navZoomIn(navObj) // `nav__i` -> navObj
-    {
-        navObj.find(".boxImg").stop(true, false).animate({
-            top: -96,
-            left: -30,
-            width:124,
-            height:124
-        }, 400);
-    }
-
-
-    // Timer
-    window.setInterval(function () {
-        
-        // Hide previous slide
-        $('.slider_bg_' + String(sliderActive) ).hide();
-
-        // Previous navigation item handling
-        var navObj = $('.slider__nav .nav__i:nth-child(' + String(sliderActive) + ')');
-        navObj.removeClass('cur');
-
-        if ( !navObj.hasClass('mouse_over') )
-        {
-            navZoomOut(navObj);
-        }
-
-        // Calculate the next slide id
-        sliderActive++;
-        if (sliderActive > sliderElementsNum)
-        {
-            sliderActive = 1;
-        }
-
-        // Show the next slide
-        $('.slider_bg_' + String(sliderActive) ).show();
-
-        // The next navigation item handling
-        var navObj = $('.slider__nav .nav__i:nth-child(' + String(sliderActive) + ')');
-        navObj.addClass('cur');
-
-        if ( !navObj.hasClass('mouse_over') )
-        {
-            navZoomIn(navObj);
-        }        
-
-        // /Switch slides and navigation items
-    }, sliderInterval);
-
-
-    // Mouse interaction with navigation toolbar
-
-    $('.nav__i').on('mouseover', function() {
-        $(this).addClass('mouse_over');
-
-        if ( !$(this).hasClass('cur') )
-        {
-            navZoomIn( $(this) );
-        }
-    });
-
-    $('.nav__i').on('mouseout', function() {
-        $(this).removeClass('mouse_over');
-
-        if ( !$(this).hasClass('cur') )
-        {
-            navZoomOut( $(this) );   
-        }
-    });
-
-
-
-    $('.slider_bg_1').click(function () {
-
-
-    } );
-
-
-
-
-=======
 $(document).ready(function(){
 
     /* Показать/Спрятать поиск */
@@ -140,7 +14,7 @@ $(document).ready(function(){
          $(".header__i").show();
      });
 
-    /* слайдер */
+    /* Свестопердящий слайдер */
     var chSpeed = 5000; //скорость смены картинок
     var nav_i = $(".nav__i");
     var slider_title = $(".slider__title");
@@ -222,7 +96,7 @@ $(document).ready(function(){
         });
     });
 
-    /* смена картинок по интервалу*/
+    /* смена картинок по интервалу, по сути просто вызов триггеров на меню */
     setInterval(function(){
         if(change){
             lastClick++;
@@ -232,7 +106,7 @@ $(document).ready(function(){
         }
     }, chSpeed);
 
-    /* закрытие поиска по клику вне блока поиска*/
+    /* закрытие поиска по клику вне блока поиска, хер пойми, но получился говнокод на скорую руку */
     $(document).on("mousedown", function(e){
         if($(e.target).closest(".header__search").length != 1){
             if($(".close_ar").hasClass("show")){
@@ -241,5 +115,4 @@ $(document).ready(function(){
         }
     });
 
->>>>>>> 88182648414bc9b3dedcb66c3859efd32d4f84dd
 }); /* END $(document).ready() */
