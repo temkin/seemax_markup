f027831bbfd6f20f73d65a30a19cfe89f9179847
Dmitry Kozhukh <kozhuhds@Dmitrys-MacBook.local>
2014-01-17 15:44:08 +0300

    validation on reg page

--- public/js/registration.js
+++ public/js/registration.js
@@ -19,6 +19,36 @@ $(document).ready(function () {
             $('.form__item .inputBox .item__i.quest').css('opacity',0.5);
         }
     );
-
+    //datepicker
     $('#choose-date').datepicker({format:"dd/mm/yyyy"});
+
+    //validation
+    $('#reg-form').bt_validate();
+
+    $.bt_validate.method(
+        'phone', 
+        function(value) {
+            return value.length == 11 && value.match(/^\d+$/);
+        },
+        "Номер телефона должен состоять из 11 цифр и включать код города и страны"
+    );  
+    $.bt_validate.method(
+        'name', 
+        function(value) {
+            return (!(/[\d]/.test(value)));
+        },
+        "Данное поле не может содержать цифры"
+    ); 
+    $.bt_validate.after_validate = function(method, value, params, result) {
+        if (!result){
+            $(this).addClass("input-error");
+        }else{
+            $(this).removeClass("input-error");
+        }
+    }
+    $.bt_validate.text["required"] = "Это поле должно быть заполнено.";
+    $.bt_validate.text["email"] = "Введен неверный e-mail.";
+    $.bt_validate.text["datetime"] = "Введена неверная дата";
+
+
 });
