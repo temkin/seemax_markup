4b96ffd57452a8ab7fd25d7d3ea9c98998dc3ca2
Dmitry Kozhukh <kozhuhds@Dmitrys-MacBook.local>
2014-01-20 16:46:35 +0300

    markets page

---
+++ public/js/markets.js
@@ -0,0 +1,6 @@
+$(document).ready(function (){
+    var citydd = new DropDown( $('#city-dd') );
+    $(document).click(function() {
+    	$('.wrapper-dropdown-1').removeClass('active');
+    });
+});
