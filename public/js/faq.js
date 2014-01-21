e35de98d3db318fbdb94df0e86f70b17e6505e82
Dmitry Kozhukh <kozhuhds@Dmitrys-MacBook.local>
2014-01-21 12:09:07 +0300

    faq validation

--- public/js/faq.js
+++ public/js/faq.js
@@ -8,5 +8,5 @@ $(document).ready(function (){
     	$(this).toggleClass('active');
     	e.preventDefault();
     });
-
+    $('#partner-form').bt_validate();
 });
--- public/js/registration.js
+++ public/js/registration.js
@@ -24,39 +24,4 @@ $(document).ready(function () {
 
     //validation
     $('#reg-form').bt_validate();
-
-    $.bt_validate.method(
-        'phone', 
-        function(value) {
-            return value.length <= 12 && value.match(/^\d+$/);
-        },
-        "Номер телефона должен состоять из 11 цифр и включать код города и страны"
-    );  
-    $.bt_validate.method(
-        'name', 
-        function(value) {
-            return (!(/[\d]/.test(value)));
-        },
-        "Данное поле не может содержать цифры"
-    ); 
-    $.bt_validate.method(
-        'serial', 
-        function(value) {
-            ;
-            return (value.replace(/\s/g, "").replace(/\-/g,"").length == 12);
-        },
-        "Данное поле заполнено неверно."
-    ); 
-    $.bt_validate.after_validate = function(method, value, params, result) {
-        if (!result){
-            $(this).addClass("input-error");
-        }else{
-            $(this).removeClass("input-error");
-        }
-    }
-    $.bt_validate.text["required"] = "Это поле должно быть заполнено.";
-    $.bt_validate.text["email"] = "Введен неверный e-mail.";
-    $.bt_validate.text["datetime"] = "Введена неверная дата";
-
-
 });
