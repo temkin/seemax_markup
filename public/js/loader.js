15ff140f9fb2d72a49ff8c55282e8654e7d4692f
Dmitry Kozhukh <kozhuhds@Dmitrys-MacBook.local>
2014-01-23 11:38:31 +0300

    some fixes on index page and product page

--- public/js/loader.js
+++ public/js/loader.js
@@ -7,9 +7,9 @@
             this.show();
             if (!this[0].startLoader){
                 this[0].startLoader = setInterval(function (){
-                    self.children().removeClass("cur").removeClass("pre-cur");
-                    self.children().eq(activePoint).addClass("cur");
-                    self.children().eq(activePoint).prev().addClass("pre-cur");
+                    self.children().removeClass("cur").removeClass("pre-cur").css('opacity',0);
+                    self.children().eq(activePoint).addClass("cur").css('opacity',1);;
+                    self.children().eq(activePoint).prev().addClass("pre-cur").css('opacity',1);;
                     activePoint++;
                     if (activePoint == 3) {
                         activePoint=0;
